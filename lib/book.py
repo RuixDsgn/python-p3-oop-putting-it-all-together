283c9eebb13be5c02b7075edfceb66f122fec59c
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-18 23:21:27 -0700

    completed lab with 2 failed tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def set_title(self):
+        return self.title
     
-        
+    def set_page(self, page_count):
+        if (type(page_count) == int):
+            self.page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size =  size
+    
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self.size = size
+
+    def cobble(self, condition = "New"):
+        self.condition = condition
+        print("Your shoe is as good as new!")
